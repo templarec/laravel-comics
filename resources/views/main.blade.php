7bebe1e87850556e125df3d121092d63a966eeba
Lorenzo <templarec@gmail.com>
2021-05-12 18:07:43 +0200

    main 1.0

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,1 +1,15 @@
-<?php
+<div class="container">
+    @foreach($film as $card)
+    <div class="card">
+
+            <div class="cover">
+                <img src="{{$card['thumb']}}" alt="image cover">
+            </div>
+            <div class="card-title">
+                <span>{{$card['title']}}</span>
+            </div>
+
+
+    </div>
+    @endforeach
+</div>
